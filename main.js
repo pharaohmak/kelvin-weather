bbeed09b9eb84551940e2ddf4f930d0e66206a95
Makram M. Ibrahim <61184060+pharaohmak@users.noreply.github.com>
2024-07-23 14:14:21 -0700

    refactored

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-// todays forcast in kelvin
-const kelvin = 0;
+function convertTemperature() {
+    const kelvin = document.getElementById('kelvin').value;
+    const celsius = kelvin - 273;
+    let fahrenheit = celsius * (9 / 5) + 32;
+    fahrenheit = Math.floor(fahrenheit);
 
-//convert kelvin to celsius
-let celsius = kelvin - 273;
-
-// convert celsius to fahrenheit
-let fahrenheit = celsius * (9 / 5) + 32;
-
-// round down fahrenheit rempreture
-fahrenheit = Math.floor(fahrenheit);
-
-console.log(`The tempreture is ${fahrenheit} degrees Fahrenheit.`);
+    document.getElementById('results').innerHTML = `
+        <p>The temperature is ${celsius} degrees Celsius.</p>
+        <p>The temperature is ${fahrenheit} degrees Fahrenheit.</p>
+    `;
+}
